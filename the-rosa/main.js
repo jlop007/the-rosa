2694cc014b721a39b8dd241448c7c15c979bbe66
Hari Om Topal <hariom1998@hotmail.com>
2020-11-12 17:40:01 +0530

    complete

--- the-rosa/main.js
+++ the-rosa/main.js
@@ -1,1 +1,45 @@
 //Select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
+// Scroll_reveal
+window.sr = ScrollReveal();
+sr.reveal('.animate-left',
+    {
+        origin: 'left',
+        duration: 1000,
+        distance: '25rem',
+        delay: 300
+
+    });
+sr.reveal('.animate-right',
+    {
+        origin: 'right',
+        duration: 1000,
+        distance: '25rem',
+        delay: 600
+
+    });
+sr.reveal('.animate-top',
+    {
+        origin: 'top',
+        duration: 1000,
+        distance: '25rem',
+        delay: 600
+
+    });
+sr.reveal('.animate-bottom',
+    {
+        origin: 'bottom',
+        duration: 1000,
+        distance: '25rem',
+        delay: 600
+
+    });
